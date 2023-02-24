2fbb1f495e060c04d36d787176953e2f310346e4
JunaidLatif <junaidlatif004@gmail.com>
2023-02-24 22:24:58 +0500

    Project INIT

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 
+// Componets :
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Pages/Home/Home';
 
+// CSS :
 import './App.scss';
 
 
 
 
 
-function App() {
+const App = () => {
 
   return (
     <>

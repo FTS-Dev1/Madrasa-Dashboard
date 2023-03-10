958308e9466dc1ac0dd8db0f52c5860e2ce2b044
osama aslam <osamaaslam029@gmail.com>
2023-03-11 03:02:12 +0500

    role actions + user searchbar designed

--- src/Pages/Dashboard/Components/Users/User.jsx
+++ src/Pages/Dashboard/Components/Users/User.jsx
@@ -20,99 +20,99 @@ import './Users.scss'
 
 const User = () => {
 
-    const [rows, setRows] = useState([
-        {
-            id: 121,
-            avatar: "Avatar",
-            name: `Osama Aslam`,
-            email: `osamaaslam029@gmail.com`,
-            phone: "03045064423",
-            role: "Admin",
-            action: "Actions",
-        },
-        {
-            id: 122,
-            avatar: "Avatar",
-            name: `Junaid Latif`,
-            email: `junaidlatif004@gmail.com`,
-            phone: "03174919167",
-            role: "Admin",
-            action: "Actions",
-        },
-        {
-            id: 123,
-            avatar: "Avatar",
-            name: `Sufyan Aslam`,
-            email: `sufyanaslam029@gmail.com`,
-            phone: "03134645127",
-            role: "User",
-            action: "Actions",
-        },
-        {
-            id: 124,
-            avatar: "Avatar",
-            name: `Zahid Ghafoor`,
-            email: `zahidghafoor@gmail.com`,
-            phone: "03004050167",
-            role: "Admin",
-            action: "Actions",
-        },
-        {
-            id: 125,
-            avatar: "Avatar",
-            name: `Hammad`,
-            email: `hadi200@gmail.com`,
-            phone: "03134645127",
-            role: "User",
-            action: "Actions",
-        },
-        {
-            id: 126,
-            avatar: "Avatar",
-            name: `Shahzaib`,
-            email: `shahzaib029@gmail.com`,
-            phone: "03045064423",
-            role: "Admin",
-            action: "Actions",
-        },
-        {
-            id: 127,
-            avatar: "Avatar",
-            name: `Zulifqar`,
-            email: `zulifqar004@gmail.com`,
-            phone: "03174919167",
-            role: "User",
-            action: "Actions",
-        },
-        {
-            id: 128,
-            avatar: "Avatar",
-            name: `Gazanfar`,
-            email: `osamaaslam029@gmail.com`,
-            phone: "03045064423",
-            role: "Admin",
-            action: "Actions",
-        },
-        {
-            id: 129,
-            avatar: "Avatar",
-            name: `Sohail`,
-            email: `zahidghafoor@gmail.com`,
-            phone: "03004050167",
-            role: "Admin",
-            action: "Actions",
-        },
-        {
-            id: 130,
-            avatar: "Avatar",
-            name: `Asad`,
-            email: `junaidlatif004@gmail.com`,
-            phone: "03174919167",
-            role: "Admin",
-            action: "Actions",
-        },
+    // const [rows, setRows] = useState([
+    //     {
+    //         id: 121,
+    //         avatar: "Avatar",
+    //         name: `Osama Aslam`,
+    //         email: `osamaaslam029@gmail.com`,
+    //         phone: "03045064423",
+    //         role: "Admin",
+    //         action: "Actions",
+    //     },
+    //     {
+    //         id: 122,
+    //         avatar: "Avatar",
+    //         name: `Junaid Latif`,
+    //         email: `junaidlatif004@gmail.com`,
+    //         phone: "03174919167",
+    //         role: "Admin",
+    //         action: "Actions",
+    //     },
+    //     {
+    //         id: 123,
+    //         avatar: "Avatar",
+    //         name: `Sufyan Aslam`,
+    //         email: `sufyanaslam029@gmail.com`,
+    //         phone: "03134645127",
+    //         role: "User",
+    //         action: "Actions",
+    //     },
+    //     {
+    //         id: 124,
+    //         avatar: "Avatar",
+    //         name: `Zahid Ghafoor`,
+    //         email: `zahidghafoor@gmail.com`,
+    //         phone: "03004050167",
+    //         role: "Admin",
+    //         action: "Actions",
+    //     },
+    //     {
+    //         id: 125,
+    //         avatar: "Avatar",
+    //         name: `Hammad`,
+    //         email: `hadi200@gmail.com`,
+    //         phone: "03134645127",
+    //         role: "User",
+    //         action: "Actions",
+    //     },
+    //     {
+    //         id: 126,
+    //         avatar: "Avatar",
+    //         name: `Shahzaib`,
+    //         email: `shahzaib029@gmail.com`,
+    //         phone: "03045064423",
+    //         role: "Admin",
+    //         action: "Actions",
+    //     },
+    //     {
+    //         id: 127,
+    //         avatar: "Avatar",
+    //         name: `Zulifqar`,
+    //         email: `zulifqar004@gmail.com`,
+    //         phone: "03174919167",
+    //         role: "User",
+    //         action: "Actions",
+    //     },
+    //     {
+    //         id: 128,
+    //         avatar: "Avatar",
+    //         name: `Gazanfar`,
+    //         email: `osamaaslam029@gmail.com`,
+    //         phone: "03045064423",
+    //         role: "Admin",
+    //         action: "Actions",
+    //     },
+    //     {
+    //         id: 129,
+    //         avatar: "Avatar",
+    //         name: `Sohail`,
+    //         email: `zahidghafoor@gmail.com`,
+    //         phone: "03004050167",
+    //         role: "Admin",
+    //         action: "Actions",
+    //     },
+    //     {
+    //         id: 130,
+    //         avatar: "Avatar",
+    //         name: `Asad`,
+    //         email: `junaidlatif004@gmail.com`,
+    //         phone: "03174919167",
+    //         role: "Admin",
+    //         action: "Actions",
+    //     },
 
-    ])
+    // ])
 
     const [data, setData] = useState([])
     const [filteredData, setFilteredData] = useState([])
@@ -132,10 +132,10 @@ const User = () => {
         },
         {
             title: 'Name',
-            dataIndex: 'name',
-            key: 'name',
+            dataIndex: 'firstName',
+            key: 'firstName',
             render: (_, data) => `${data?.firstName} ${data.lastName}`,
-            sorter: (a, b) => a.name.localeCompare(b.name),
+            sorter: (a, b) => a.firstName.localeCompare(b.firstName),
 
         },
         {
@@ -165,18 +165,12 @@ const User = () => {
             title: 'Actions',
             dataIndex: 'action',
             key: 'action',
+            align: "center",
         },
 
     ]
 
-    const searchHandler = (event) => {
-        let filteredData = rows.filter((data) =>
-            data.name.includes(event) ||
-            data.email.includes(event)
-        )
 
-        setFilteredData(filteredData)
-    }
 
     const gettingAllUsers = async () => {
         setLoading(true)
@@ -192,6 +186,15 @@ const User = () => {
     useEffect(() => {
         gettingAllUsers()
     }, [])
+
+    const searchHandler = (event) => {
+        let filteredData = data.filter((a) =>
+            a.name?.toLowerCase()?.includes(event) ||
+            a.email?.toLowerCase()?.includes(event)
+        )
+
+        setFilteredData(filteredData)
+    }
 
     return (
 

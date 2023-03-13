22eb3a8afd81f7b5f26692100f06e2166dd126d5
osama aslam <osamaaslam029@gmail.com>
2023-03-13 19:22:12 +0500

    save blogs loading button added

--- src/Pages/Dashboard/Components/Blogs/Blogs.jsx
+++ src/Pages/Dashboard/Components/Blogs/Blogs.jsx
@@ -1,19 +1,8 @@
 import React, { useState, useEffect } from 'react'
 
-// MUI | ANT-D :
-import { Popover, Input, Button } from 'antd';
-
 // Components
 import AllBlogs from "./AllBlogs/AllBlogs"
 import EditBlog from "./EditBlog"
-
-// Assets | ICONS :
-import { ReactComponent as Save } from '../../../../Assets/Post/save.svg'
-
-// API :
-import { CreatBlogsAPI } from '../../../../API/blogs';
-// Helpres :
-import { toast } from "react-toastify";
 
 // CSS :
 import './Blogs.scss'
